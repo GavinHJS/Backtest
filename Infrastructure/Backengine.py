a93b2bb27062c874929cb7458936b6ae8d90f5c8
Gavin Hui <huijiashun@hotmail.com>
2021-02-24 13:22:33 +0800

    added main

--- Infrastructure/Backengine.py
+++ Infrastructure/Backengine.py
@@ -7,43 +7,41 @@ Created on Tue Feb 23 21:07:17 2021
 Note:
         Date is in YYYY-MM-DD format
 """
-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 import backtrader as bt
 import sys
+import datetime
+import matplotlib.pyplot as plt
 
-sys.path.insert(1, "'D:\\Backtest\\Data'")
+sys.path.insert(1, "..\\Backtest\\Data")
 
 from EquityData import dataPreperation
+import plotly
 
 class backEngine:
     
-    def __init__(self , tickerData,date1 , date2 , frequency):
-        self.data = dataPreperation(tickerData , date1,date2 , frequency).getHistoricalData()
-        cerebro = bt.Cerebro()
+    def __init__(self , tickerData,date1 , date2 , frequency , notional ):
+        self.data       = dataPreperation(tickerData , date1,date2 , frequency).getHistoricalData()
+        self.notional   = notional 
         
-
-
-    # cerebro = bt.Cerebro()
-
-    # data = bt.feeds.YahooFinanceCSVData(
-    #     dataname=x,
-    #     # Do not pass values before this date
-    #     fromdate=datetime.datetime(2000, 1, 1),
-    #     # Do not pass values after this date
-    #     todate=datetime.datetime(2000, 12, 31),
-    #     reverse=False)
-
-    # # Add the Data Feed to Cerebro
-    # cerebro.adddata(data)
-
-    # # Set our desired cash start
-    # cerebro.broker.setcash(100000.0)
-    # print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-    # cerebro.run()
-
-    # print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    def setEngine(self):
+        self.cerebro     = bt.Cerebro()
+        data             = bt.feeds.PandasData(dataname = self.data) 
+        self.cerebro.adddata(data)
+        self.cerebro.broker.set_cash(self.notional)
+        
+    def runEngine(self):
+        print('Starting Portfolio Value: %.2f' % self.cerebro.broker.getvalue())
+        self.cerebro.run()
+        print('Final Portfolio Value: %.2f' % self.cerebro.broker.getvalue())
     
-
+    def activate(self):
+        self.setEngine()
+        self.runEngine()
+        
 if __name__ =="__main__":
-    x = backEngine("MSFT" , "2020-01-01" , "2021-01-01", "1d")
+    x = backEngine("MSFT" , "2020-01-01" , "2021-01-01", "1d" , 1000)
+    
+    
+    
---
+++ app/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 12:51:16 2021
+
+@author: Gavin
+"""
+import sys
+sys.path.insert(0, "..\\Backtest\\Data")
+sys.path.insert(1, "..\\Backtest\\Infrastructure")
+
+from Backengine import backEngine
+from EquityData import dataPreperation
+
+x =backEngine("AAPL" , "2020-01-01" , "2021-01-01", "1d" , 1000)
+
+x.activate()
