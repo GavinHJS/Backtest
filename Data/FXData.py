8a883cbc4688b046f73453984bfddbfdbeb08b13
Gavin Hui <huijiashun@hotmail.com>
2021-03-05 13:13:47 +0800

    inserted fx data

--- Data/FXData.py
+++ Data/FXData.py
@@ -5,3 +5,28 @@ Created on Tue Feb 23 20:33:45 2021
 @author: Gavin
 """
 
+import pandas as pd
+from datetime import datetime
+import yfinance as yf
+
+class fxData():
+    def __init__(self,fx,startDate , endDate , frequency):
+        if len(fx) != 5:
+            raise ValueError("Invalid Fx Format. Try e.g. EUR=X")
+        else:
+            self.fxPair = fx
+            
+        self.startDate = startDate
+        self.endDate = endDate
+        self.frequency = frequency
+        
+        
+    def getData(self):
+        historicalData = yf.Ticker(self.fxPair)
+        historicalData.history()
+        
+        
+        
+
+if __name__ == "__main__":
+    x = fxData("EUR=X" , "2020-01-01" , "2021-01-01", "1d")
